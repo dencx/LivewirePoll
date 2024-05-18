c2ca37496c4e549be3245adcaebadbb812d04220
Denys Bieliaiev <81828135+dencx@users.noreply.github.com>
2024-05-19 00:15:10 +0200

    add actions in livewire

--- resources/views/livewire/create-poll.blade.php
+++ resources/views/livewire/create-poll.blade.php
@@ -3,5 +3,15 @@
         <label>Poll title</label>
         <input type="text" wire:model="title" />
         Current title: {{ $title }}
+        <div class="mb-4 mt-4">
+            <button class="btn" wire:click.prevent="addOption">Add option</button>
+        </div>
+        <div>
+            @foreach ($options as $index => $option)
+                <div class="mb-4">
+                    {{ $index }} - {{ $option }}
+                </div>
+            @endforeach
+        </div>
     </form>
 </div>
