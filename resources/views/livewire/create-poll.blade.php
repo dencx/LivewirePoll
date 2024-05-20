868944057d88821f82f7d89bf37308e4680c48eb
Denys Bieliaiev <81828135+dencx@users.noreply.github.com>
2024-05-20 08:50:33 +0200

    add validation

--- resources/views/livewire/create-poll.blade.php
+++ resources/views/livewire/create-poll.blade.php
@@ -2,19 +2,26 @@
     <form wire:submit.prevent="createPoll">
         <label>Poll title</label>
         <input type="text" wire:model="title" />
+
+        @error('title')
+            <div class="text-red-500">{{ $message }}</div>
+        @enderror
+
         <div class="mb-4 mt-4">
             <button class="btn" wire:click.prevent="addOption">Add option</button>
         </div>
         <div>
             @foreach ($options as $index => $option)
                 <div class="mb-4">
-                    <label for="">Option {{$index + 1}}</label>
+                    <label for="">Option {{ $index + 1 }}</label>
                     <div class="flex gap-2">
-                        <input type="text" wire:model="options.{{$index}}"/>
-                        <button class="btn" wire:click.prevent="removeOption({{$index}})">Remove</button>
+                        <input type="text" wire:model="options.{{ $index }}" />
+                        <button class="btn" wire:click.prevent="removeOption({{ $index }})">Remove</button>
                     </div>
+                    @error("options.{$index}")
+                            <div class="text-red-500">{{ $message }}</div>
+                        @enderror
                 </div>
-                
             @endforeach
         </div>
         <button type="submit" class="btn">Create Poll</button>
